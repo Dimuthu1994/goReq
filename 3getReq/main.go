647c0ffab1d2db2a941e7add2b6edc458d5a732a
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-03-19 16:56:03 +0530

    post req with JSON data

--- 3getReq/main.go
+++ 3getReq/main.go
@@ -32,3 +32,25 @@ func performGetReq(){
 	//fmt.Println(content)
 	//fmt.Println(stringContent)
 }
+
+func performPostJsonReq(){
+	const myurl = "http://localhost:8000/post"
+
+	reqBody := strings.NewReader(`
+	{
+		"courseName":"React",
+		"price":0,
+		"Instructor":"Dimuthu"
+	}
+	`)
+
+	res,err := http.Post(myurl,"application/json",reqBody)
+
+	if err != nil{
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	content,_ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(content))
+}
