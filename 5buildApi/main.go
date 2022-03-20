1e7075dd7e4c09375fcdadf3e2017aeced8da4c3
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-03-20 20:04:19 +0530

    build contollers to handle get req

--- 5buildApi/main.go
+++ 5buildApi/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
@@ -18,4 +24,15 @@ var courses []Course
 //middleware
 func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
+}
+
+//handle get req
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to API by Dimuthu</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all Courses")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
 }
