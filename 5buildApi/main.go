799cc6a25c7a6d5f9e9f531978b16b0e685fc00b
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-03-20 21:10:32 +0530

    add a course

--- 5buildApi/main.go
+++ 5buildApi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -25,7 +28,7 @@ var courses []Course
 
 //middleware
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	return  c.CourseName == ""
 }
 
 //handle get req
@@ -53,5 +56,32 @@ func getOneCourse(w http.ResponseWriter, r *http.Request){
 		}
 	}
 	json.NewEncoder(w).Encode("No course found on given id")
+	return
+}
+
+func addCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("add a Course")
+	w.Header().Set("Content-Type","application/json")
+	
+	if r.Body == nil{
+		json.NewEncoder(w).Encode("please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	if course.IsEmpty(){
+		json.NewEncoder(w).Encode("No data in JSON")
+		return
+	}
+
+	//generate unique id
+	//id to string
+	//append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
 	return
 }
